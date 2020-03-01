c136304284fab8ab611a1540576ceeef458535e1
Kyle McCurley <kmccurley123@gmail.com>
2020-02-29 17:02:00 -0800

    Create hello_sophie_part_1.rb

---
+++ hello_sophie_part_1.rb
@@ -0,0 +1,13 @@
+=begin
+Using the code from the previous exercise, add a parameter to #initialize that provides a name for the Cat object.
+Use an instance variable to print a greeting with the provided name. (You can remove the code that displays I'm a cat!.)
+=end
+
+class Cat
+  def initialize(name)
+    puts "I'm a cat!"
+    @name = name
+  end
+end
+
+kitty = Cat.new('Sophie')
