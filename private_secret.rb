e8c5a5bf6768f9359deef441428cbae077e790ee
Kyle McCurley <kmccurley123@gmail.com>
2020-02-29 18:13:36 -0800

    Create private_secret.rb

---
+++ private_secret.rb
@@ -0,0 +1,13 @@
+class Person
+  attr_writer :secret
+  
+  def share_secret; secret; end
+
+  private
+
+  attr_reader :secret
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+person1.share_secret
