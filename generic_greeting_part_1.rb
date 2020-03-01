c1278b9669989d2a0b53693eb4722b7a164acd84
Kyle McCurley <kmccurley123@gmail.com>
2020-02-29 17:30:44 -0800

    Create generic_greeting_part_1.rb

---
+++ generic_greeting_part_1.rb
@@ -0,0 +1,7 @@
+# Modify the following code so that Hello! I'm a cat! is printed when Cat.generic_greeting is invoked.
+
+class Cat
+  def self.generic_meeting; puts 'Hello! I'm a cat!'; end
+end
+
+Cat.generic_greeting
